73965cd94c3a0cdd5371199c7fd3daf3c13054a7
Adalid777 <115057872+Adalid777@users.noreply.github.com>
2022-10-05 21:51:24 -0400

    Add files via upload

---
+++ Multiplicador.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+public class Multiplicador {
+    private int resultado;
+    
+    public Multiplicador(){
+   resultado=0;   
+}
+     public int multiplicar(){
+    int N1=86;
+    int N2=23;
+    
+    resultado=N1*N2;
+    System.out.println("El resultado de la multiplicacion de "+N1+" y "+N2+ " es: "+resultado);
+ return resultado;
+     }    
+}
+
+
+
+
