1390ce85d7f63f5d1b04d356cdbda9a5dc1d3ffa
IvanD-A <84353402+IvanD-A@users.noreply.github.com>
2022-10-05 22:30:26 -0400

    Add files via upload

---
+++ ResyDiv.java
@@ -0,0 +1,9 @@
+public class ResyDiv
+{
+    public void resta(int val1, int val2){
+        System.out.print(val1-val2);
+    }
+    public void division(int val1, int val2){
+        System.out.print(val1/val2);
+    }
+}
