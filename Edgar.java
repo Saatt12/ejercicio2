c55ad54758b56bcf15eb3a779887bed3f35626ae
ensy <010lobo010@gmail.com>
2022-10-05 23:26:26 -0400

    commit edgar

---
+++ Edgar.java
@@ -0,0 +1,12 @@
+//EDGAR FAUSTO COPA LOPEZ
+public class Edgar{
+	public static void main(String args[]){
+	
+		System.out.print("hola, mundo");
+	}
+	
+	public void suma(int a,int b){
+		
+		int s=a+b;
+	}
+}
